4a8281d35eecba55a73f390f8bbdf3585816374c
HieuNguyen0901 <nxhieugl0901@gmail.com>
2022-12-14 16:49:56 +0700

    uState - 2

--- pages/form.tsx
+++ pages/form.tsx
@@ -2,14 +2,35 @@ import { useState } from 'react'
 
 function learn_useState(){
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [counter, setCounter] = useState(1)
-    const handleIncrease = () =>{
-        setCounter(counter + 1)
+    const [info, setInfo] = useState({
+        name:'Nguyen Van A',
+        age: '18'
+    })
+    
+    const handleUpdate = () =>{
+        // setInfo({
+        //     ...info,
+        //     bio: 'Yeu mau hong'
+        // })
+
+        // setInfo(prev => ({
+        //     ...prev,
+        //     bio: 'Yeu mau hong'
+        // }))
+
+        setInfo(prev => {
+            //logic
+            return{
+                ...prev,
+                bio: 'Yeu mau hong'    
+            }
+        })
     }
+    
     return(
         <div className="App" style={{ padding:20 }}>
-            <h1>{counter}</h1>
-            <button onClick={handleIncrease}>Increase</button>
+            <h1>{JSON.stringify(info)}</h1>
+            <button onClick={handleUpdate}>Update</button>
         </div>
     );
 }
